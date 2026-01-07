925780537e71774d5f3d3146daae930f34ebf5c8
MelchiorN <ysylvain16@gmail.com>
2026-01-07 10:42:32 +0000

    Création des packages,classes

--- src/main/java/com/bank/ega/repository/TransactionRepository.java
+++ src/main/java/com/bank/ega/repository/TransactionRepository.java
@@ -1,5 +1,22 @@
 package com.bank.ega.repository;
 
-public interface TransactionRepository {
+import com.bank.ega.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByCompteSourceIdOrCompteDestinationId(
+            Long sourceId,
+            Long destinationId
+    );
+
+    List<Transaction> findByDateTransactionBetween(
+            LocalDateTime debut,
+            LocalDateTime fin
+    );
 }
--- src/main/java/com/bank/ega/repository/UtilisateurRepository.java
+++ src/main/java/com/bank/ega/repository/UtilisateurRepository.java
@@ -1,5 +1,13 @@
 package com.bank.ega.repository;
 
-public interface UtilisateurRepository {
+import com.bank.ega.entity.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+
+    Optional<Utilisateur> findByUsername(String username);
 }
